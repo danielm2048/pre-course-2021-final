a428dcda84fa806053c42945c3a154a0cc6bfe31
danielm2048 <danielmimoun98@gmail.com>
2021-01-28 12:41:27 +0200

    Added sorting by priority

--- src/main.js
+++ src/main.js
@@ -1,48 +1,88 @@
-const todoList = document.querySelector("#todo-list");
+let todoList = document.querySelector("#todo-list");
 const todoInput = document.querySelector("#text-input");
 const priorities = document.querySelector("#priority-selector");
-const addTodo = document.querySelector("#add-button");
+const todoButton = document.querySelector("#add-button");
+const todos = [];
 
 const counter = document.querySelector("#counter");
+const sortButton = document.querySelector("#sort-button");
 
-addTodo.addEventListener("click", (e) => {
+todoButton.addEventListener("click", () => {
   if (todoInput.value) {
-    const listItem = document.createElement("li");
 
-    const todoContainer = document.createElement("div");
-    todoContainer.classList.add("todo-container");
+    const todoCreatedAt = formatDate(new Date());
+    const todoPriority = priorities.value;
+    const todoText = todoInput.value;
 
-    const todoCreatedAt = document.createElement("div");
-    todoCreatedAt.classList.add("todo-created-at");
-    todoCreatedAt.textContent = formatDate(new Date());
-    todoContainer.appendChild(todoCreatedAt);
+    todoList.appendChild(addTodo(todoCreatedAt, todoPriority, todoText));
 
-    const todoPriority = document.createElement("div");
-    todoPriority.classList.add("todo-priority");
-    todoPriority.textContent = priorities.value;
-    todoContainer.appendChild(todoPriority);
-
-    const todoText = document.createElement("div");
-    todoText.classList.add("todo-text");
-    todoText.textContent = todoInput.value;
-    todoContainer.appendChild(todoText);
+    todos.push({
+      todoCreatedAt,
+      todoPriority,
+      todoText
+    });
 
     counter.textContent++;
 
     todoInput.value = "";
     todoInput.focus();
-    listItem.appendChild(todoContainer);
-    todoList.appendChild(listItem);
   }
 });
 
 // Execute onClick button function when the user releases the enter key
 todoInput.addEventListener("keyup", (e) => {
-  if (e.code === "Enter") {
+  if (e.code === "Enter" || e.code === "NumpadEnter") {
     e.preventDefault();
-    addTodo.click();
+    todoButton.click();
   }
 });
+
+sortButton.addEventListener("click", () => {
+  sortList("todoPriority");
+})
+
+// This function creates a list item and returns it
+const addTodo = (date, priority, text) => {
+  const listItem = document.createElement("li");
+
+  const todoContainer = document.createElement("div");
+  todoContainer.classList.add("todo-container");
+
+  const todoCreatedAt = document.createElement("div");
+  todoCreatedAt.classList.add("todo-created-at");
+  todoCreatedAt.textContent = date;
+  todoContainer.appendChild(todoCreatedAt);
+
+  const todoPriority = document.createElement("div");
+  todoPriority.classList.add("todo-priority");
+  todoPriority.textContent = priority;
+  todoContainer.appendChild(todoPriority);
+
+  const todoText = document.createElement("div");
+  todoText.classList.add("todo-text");
+  todoText.textContent = text;
+  todoContainer.appendChild(todoText);
+
+  listItem.appendChild(todoContainer);
+  return listItem;
+}
+
+// A function to sort todo list by property
+const sortList = (prop) => {
+  const parent = todoList.parentNode;
+  parent.removeChild(todoList);
+  todoList = document.createElement("ul");
+
+  todos.sort((a, b) => {
+    return b[prop] - a[prop];
+  });
+
+  todos.forEach(todo => {
+    todoList.appendChild(addTodo(todo.todoCreatedAt, todo.todoPriority, todo.todoText));
+  });
+
+  parent.appendChild(todoList);
+}
 
 // A function to format dates in SQL format
 const formatDate = (date) => {
