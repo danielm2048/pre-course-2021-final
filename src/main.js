60bce180de34261cc3492e000ba4774d2f658682
danielm2048 <danielmimoun98@gmail.com>
2021-01-29 22:50:07 +0200

    Added checkmark for each todo

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const sortButton = document.querySelector("#sort-button");
 let todos = [];
 
 // This function creates a list item and returns it
-const addTodo = (date, priority, text) => {
+const addTodo = (date, priority, text, index) => {
   const listItem = document.createElement("li");
 
   const todoContainer = document.createElement("div");
@@ -32,6 +32,10 @@ const addTodo = (date, priority, text) => {
   todoText.textContent = text;
   todoContainer.appendChild(todoText);
 
+  const todoCheck = createCheckmark(index);
+  todoCheck.classList.add("todo-check");
+  todoContainer.appendChild(todoCheck);
+
   listItem.appendChild(todoContainer);
   return listItem;
 }
@@ -45,9 +49,8 @@ window.addEventListener("load", async () => {
   loading.style.display = "none";
   container.style.display = "block";
 
-  console.log(todos);
-  todos.forEach(todo => {
-    todoList.appendChild(addTodo(todo.date, todo.priority, todo.text));
+  todos.forEach((todo, index) => {
+    todoList.appendChild(addTodo(todo.date, todo.priority, todo.text, index));
   });
 
   counter.textContent = todos.length;
@@ -99,8 +102,8 @@ const sortList = (prop) => {
     return b[prop] - a[prop];
   });
 
-  todos.forEach(todo => {
-    todoList.appendChild(addTodo(todo.date, todo.priority, todo.text));
+  todos.forEach((todo, index) => {
+    todoList.appendChild(addTodo(todo.date, todo.priority, todo.text, index));
   });
 
   parent.appendChild(todoList);
@@ -113,4 +116,39 @@ const formatDate = (date) => {
   const hour = date.getHours() < 10 ? "0" + (date.getHours() + 1) : date.getHours();
   const minute = date.getMinutes() < 10 ? "0" + (date.getMinutes() + 1) : date.getMinutes();
   return `${date.getFullYear()}-${month}-${day} ${hour}:${minute}`;
+}
+
+// A function for creating a checkmark
+const createCheckmark = (index) => {
+  const todoCheck = document.createElement("div");
+  const checkBox = document.createElement("input");
+  const checkBoxLabel = document.createElement("label");
+  const wrapper = document.createElement("div");
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("id", `item-${index}`);
+
+  checkBoxLabel.setAttribute("for", `item-${index}`);
+  checkBoxLabel.classList.add("button");
+
+  wrapper.classList.add("wrapper");
+
+  svg.setAttribute("x", "0px");
+  svg.setAttribute("y", "0px");
+  svg.setAttribute("viewBox", "0 0 98.5 98.5");
+  svg.setAttribute("enable-background", "new 0 0 98.5 98.5");
+
+  path.classList.add("checkmark");
+  path.setAttribute("stroke-width", "8");
+  path.setAttribute("stroke-miterlimit", "10");
+  path.setAttribute("d", "M81.7,17.8C73.5,9.3,62,4,49.2,4C24.3,4,4,24.3,4,49.2s20.3,45.2,45.2,45.2s45.2-20.3,45.2-45.2c0-8.6-2.4-16.6-6.5-23.4l0,0L45.6,68.2L24.7,47.3");
+
+  svg.appendChild(path);
+  wrapper.appendChild(svg);
+  todoCheck.appendChild(checkBox);
+  todoCheck.appendChild(checkBoxLabel)
+  todoCheck.appendChild(wrapper);
+  return todoCheck;
 }
