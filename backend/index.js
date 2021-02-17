cd8316fbd3d1578fefd5da17932572a48230db99
danielm2048 <danielmimoun98@gmail.com>
2021-02-17 21:18:25 +0200

    Added route file to index

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const binRoutes = require("./routes/v1/bin");
+
 app.use(express.json());
+app.use("/api", binRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
